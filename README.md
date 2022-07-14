921d5dd0d6de9db142d701dbf4843c23ab05cd1e
minasanta <minasanta77@gmail.com>
2022-07-14 02:29:35 +0200

    final one

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# -Udacity-landing-page
-This's Landing Page the ptoject of FWD Professional Front-End Web
+# Landing Page Project
+This's Landing Page the ptoject of FWD
+## What I did
+
+* I used the template of Udacity and I didn't changed many things  
+* I used the fragment ti imporve the time and performance 
+* I used CSS scroll smooth
+* I used the scroll window with getBoundingClientRect() function to active a section is in the viewport
---
+++ js/app.js
@@ -0,0 +1,98 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const sections = Array.from(document.querySelectorAll("section"));
+const navBar = document.getElementById("navbar__list");
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+function createNavLinks()
+{
+    const fragment = document.createDocumentFragment();
+
+    sections.forEach((section) => {
+        const list = document.createElement('li');
+        const link = document.createElement('a');
+
+        list.className = "list-item";
+        fragment.appendChild(list);
+
+        link.textContent = section.dataset.nav;
+        link.className = "menu__link";
+        link.href = `#${section.id}`;
+        list.appendChild(link);
+    });
+    navBar.appendChild(fragment);
+}
+
+// build the nav
+
+createNavLinks()
+
+// Add class 'active' to section when a link is clicked
+
+const links = Array.from(document.querySelectorAll(".menu__link"));
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        for (const section of sections) 
+        {
+            if(section.dataset.nav === link.textContent)
+            {
+                section.className = 'your-active-class';
+            }
+            else 
+            {
+                section.className = '';
+            }
+        }
+    });
+});
+
+// Add class 'active' to section when near top of viewport
+
+window.onscroll = () => {
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top >= -150 && rect.top <= 400)
+        {
+            section.className = 'your-active-class';
+        }
+        else 
+        {
+            section.className = '';
+        }
+    });
+};
+
+
+
